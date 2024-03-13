4ff02895717c50a14f928a7a7dc8938ed0830574
Mayonesio <jara.mayonesio@gmail.com>
2024-03-13 08:47:58 +0100

    frontend_login002_setup layout of origin

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,16 +1,20 @@
+/**
+ * Renders a login form with email and password inputs, a submit button, and a link to reset the password.
+ * @returns {JSX.Element} The login form component.
+ */
 const Login = () => {
-    return (
-        <div>
-        <div className="h-screen w-full bg-cover" style={{backgroundImage: "url('/images/Image_Header1_49d601fa4c.jpg')"}}>
-        <div className="h-full w-full flex items-center justify-center">
-        <div className="h-full w-full">
-          <div className="flex flex-col hover:blur-0 h-full bg-center bg-cover bg-[#08215C] items-center justify-center w-full gap-5 opacity-90">
+  return (
+    <div className="h-screen w-full bg-cover relative">
+      <div className="absolute inset-0" style={{ backgroundImage: "url('/images/Image_Header1_49d601fa4c.jpg')", backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+      <div className="absolute inset-0 bg-[#08215C] opacity-80"></div>
+      <div className="h-full flex items-center justify-center">
+        <div className="w-full">
+          <div className="flex flex-col hover:blur-0 bg-center bg-cover items-center justify-center w-full gap-5 relative z-10">
             <h1 className="my-6">
-              <img src='/images/incoBLanco.svg' />
+              <img src='/images/incoBLanco.svg' alt="Logo" />
             </h1>
             <ul className="inline-flex items-center text-xl gap-10">
               {/* List Items */}
-              
             </ul>
             <p className="text-white text-right">or use email your account</p>
             <input
@@ -26,7 +30,7 @@ const Login = () => {
             <div className="text-right">
               <a
                 href=""
-                className="italic text-white/50 text-sm underline decoration-violet-500 text-violet-500 hover:text-white  transition"
+                className="italic text-white/50 text-sm underline decoration-violet-500 text-violet-500 hover:text-white transition"
               >
                 Forget your Password?
               </a>
@@ -39,9 +43,8 @@ const Login = () => {
           </div>
         </div>
       </div>
-    </div>           
     </div>
-    )
+  );
 }
 
 export default Login
