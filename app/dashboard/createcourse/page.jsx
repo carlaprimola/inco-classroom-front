7c320110f1532c67e3873d4e6616334e7b10b250
soyshan <shanbu131@gmail.com>
2024-03-20 12:29:35 +0100

    frontend_CRUD001_Creating the CRUD of courses

---
+++ app/dashboard/createcourse/page.jsx
@@ -0,0 +1,147 @@
+// pagina de configuracion cursos
+"use client";
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import Link from "next/link";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+
+// export default function CourseSettings() {
+//     const [cursos, setCursos] = useState([]);
+
+//     useEffect(() => {
+//         const fetchCursos = async () => {
+//             try {
+//                 const response = await axios.get("http://localhost:8000/cursos");
+//                 console.log("Datos de cursos recibidos:", response.data);
+//                 setCursos(response.data);
+//             } catch (error) {
+//                 console.error("Error fetching cursos:", error);
+//             }
+//         };
+
+//         fetchCursos();
+//     }, []);
+
+//     // Función para eliminar curso
+//     const deleteCurso = async (cursoID) => {
+//         try {
+//             await axios.delete(`http://localhost:8000/cursos/${cursoID}`);
+//             // Actualizar la lista de cursos después de eliminar uno
+//             fetchCursos();
+//         } catch (error) {
+//             console.error('Error deleting curso:', error);
+//         }
+//     };
+
+//     return (
+//         <main>
+//             <h1 className="text-black">Administración de Cursos</h1>
+//             <table>
+//                 <thead>
+//                     <tr>
+//                         <th className="text-black">Nombre del Curso</th>
+//                         <th className="text-black">Imagen</th>
+//                         <th className="text-black">ID del Contenido del Curso</th>
+//                         <th className="text-black">Docente</th>
+//                         <th className="text-black">Duración</th>
+//                         <th className="text-black">Acciones</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     {cursos.map(curso => (
+//                         <tr key={curso.ID}>
+//                             <td className="text-black">{curso.NombreCurso}</td>
+//                             <td className="text-black">{curso.imageUrl}</td>
+//                             <td className="text-center text-black">{curso.contenidocurso_ID}</td>
+//                             <td className="text-black">{curso.Docente}</td>
+//                             <td className="text-black">{curso.Duracion}</td>
+//                             <td>
+//                                 <Link href={`/edit/${curso.ID}`}>
+//                                     <button className=" w-10 h-10 text-center justify-center button-courses flex items-center text-white mb-3  bg-blue-500"><FontAwesomeIcon icon={faPenToSquare} /></button>
+//                                 </Link>
+//                                 <button className="w-10 h-10 text-center justify-center button-courses flex items-center text-white mb-3 bg-red-500" onClick={() => deleteCurso(curso.ID)}><FontAwesomeIcon icon={faTrash} /></button>
+//                             </td>
+//                         </tr>
+//                     ))}
+//                 </tbody>
+//             </table>
+//         </main>
+//     );
+// }
+
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrash, faPlus } from "@fortawesome/free-solid-svg-icons";
+
+export default function CourseSettings() {
+    const [cursos, setCursos] = useState([]);
+
+    useEffect(() => {
+        fetchCursos();
+    }, []);
+
+    const fetchCursos = async () => {
+        try {
+            const response = await axios.get("http://localhost:8000/cursos");
+            console.log("Datos de cursos recibidos:", response.data);
+            setCursos(response.data);
+        } catch (error) {
+            console.error("Error fetching cursos:", error);
+        }
+    };
+
+    const deleteCurso = async (id) => {
+        try {
+            await axios.delete(`http://localhost:8000/cursos/${id}`);
+            // Actualizar la lista de cursos después de eliminar uno
+            const updatedCursos = cursos.filter(curso => curso.ID !== id);
+            setCursos(updatedCursos);
+        } catch (error) {
+            console.error('Error deleting curso:', error);
+        }
+    };
+
+    return (
+        <main>
+            <h1 className="text-black">Administración de Cursos</h1>
+            <div className="flex justify-center mb-3">
+                <Link href="/dashboard/createcourse">
+                    <button className="w-10 h-10 text-center justify-center button-courses flex items-center text-white bg-blue-500 mr-3"><FontAwesomeIcon icon={faPlus} /></button>
+                </Link>
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th className="text-black">Nombre del Curso</th>
+                        <th className="text-black">Imagen</th>
+                        <th className="text-black">ID del Contenido del Curso</th>
+                        <th className="text-black">Docente</th>
+                        <th className="text-black">Duración</th>
+                        <th className="text-black">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cursos.map(curso => (
+                        <tr key={curso.ID}>
+                            <td className="text-black">{curso.NombreCurso}</td>
+                            <td className="text-black">{curso.imageUrl}</td>
+                            <td className="text-center text-black">{curso.contenidocurso_ID}</td>
+                            <td className="text-black">{curso.Docente}</td>
+                            <td className="text-black">{curso.Duracion}</td>
+                            <td>
+                                <Link href="/dashboard/editcourse">
+                                    <button className="w-10 h-10 text-center justify-center button-courses flex items-center text-white bg-blue-500 mr-3"><FontAwesomeIcon icon={faPenToSquare} /></button>
+                                </Link>
+                                <button className="w-10 h-10 text-center justify-center button-courses flex items-center text-white bg-red-500" onClick={() => deleteCurso(curso.ID)}><FontAwesomeIcon icon={faTrash} /></button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </main>
+    );
+}
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,58 +1,58 @@
-// 'use client';
+'use client';
 
-// import React, { useState, useEffect } from 'react';
-// import { fetchUsers } from "/app/lib/data.js"; // Importa la función fetchUsers
-// import Search from "/app/ui/dashboard/search/search";
-// import styles from "/app/ui/dashboard/users/users.module.css";
-// // Importa Image de 'next/image' si es necesario
-// import Link from "next/link";
+import React, { useState, useEffect } from 'react';
+import { fetchUsers } from "/app/lib/data.js"; // Importa la función fetchUsers
+import Search from "/app/ui/dashboard/search/search";
+import styles from "/app/ui/dashboard/users/users.module.css";
+// Importa Image de 'next/image' si es necesario
+import Link from "next/link";
 
-// const UsersPage = ({ searchParams }) => {
-//     const [users, setUsers] = useState([]);
+const UsersPage = ({ searchParams }) => {
+    const [users, setUsers] = useState([]);
 
-//     useEffect(() => {
-//         const loadUsers = async () => {
-//             try {
-//                 const usersData = await fetchUsers();
-//                 setUsers(usersData); // Asigna directamente los datos de usuarios al estado de usuarios
-//                 console.log(usersData)
-//             } catch (error) {
-//                 console.error('Error fetching users:', error);
-//             }
-//         };
+    useEffect(() => {
+        const loadUsers = async () => {
+            try {
+                const usersData = await fetchUsers();
+                setUsers(usersData); // Asigna directamente los datos de usuarios al estado de usuarios
+                console.log(usersData)
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        };
 
 
-//         loadUsers();
-//     }, [users]);
+        loadUsers();
+    }, [users]);
 
-//     // return (
-//         // <div className={styles.container}>
-//         //     <div className={styles.top}>
-//         //         <Search placeholder="Search for a user..." />
-//         //         <Link className='text-black' href="/dashboard/users/add">
-//         //             <button className= {styles.addButton}>Add New</button>
-//         //         </Link>
-//         //     </div>
-//         //     <div className="bg-gray-100 min-h-screen py-16">
-//         //         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-//         //             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-//         //                 {users.map(user => (
-//         //                     <div key={user.ID} className="bg-white rounded-lg shadow-md overflow-hidden">
-//         //                         {/* Agrega las propiedades del usuario que deseas mostrar */}
-//         //                         <div className="p-6 text-black ">
-//         //                             <h2 className="text-xl font-semibold mb-2">{user.Nombre}</h2>
-//         //                             <p>Email: {user.Email}</p>
-//         //                             {/* Mostrar el rol del usuario */}
-//         //                             <p>Rol: {user.role.TipoRol}</p>
-//         //                         </div>
-//         //                     </div>
-//         //                 ))}
-//         //             </div>
-//         //         </div>
-//         //     </div>
-//         // </div>
-//     // );
-// };
+    return (
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <Search placeholder="Search for a user..." />
+                <Link className='text-black' href="/dashboard/users/add">
+                    <button className= {styles.addButton}>Add New</button>
+                </Link>
+            </div>
+            <div className="bg-gray-100 min-h-screen py-16">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                        {users.map(user => (
+                            <div key={user.ID} className="bg-white rounded-lg shadow-md overflow-hidden">
+                                {/* Agrega las propiedades del usuario que deseas mostrar */}
+                                <div className="p-6 text-black ">
+                                    <h2 className="text-xl font-semibold mb-2">{user.Nombre}</h2>
+                                    <p>Email: {user.Email}</p>
+                                    {/* Mostrar el rol del usuario */}
+                                    <p>Rol: {user.role.TipoRol}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-// export default UsersPage;
+export default UsersPage;
 
