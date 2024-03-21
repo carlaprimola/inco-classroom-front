49578e471eed08ab6b5419e0a5ca550735b03572
Mayonesio <jara.mayonesio@gmail.com>
2024-03-21 21:03:59 +0100

    frontend_dashboard009_Home changed for some text, and button added to the sidebar, waiting for functionality

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,8 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-import { fetchUsers } from "/app/lib/data.js"; // Importa la función fetchUsers
+import { fetchUsers } from "/app/lib/data.js"; 
+import MainInfo from "../ui/dashboard/mainInfo/MainInfo"
+
 import Search from "/app/ui/dashboard/search/search";
 import styles from "/app/ui/dashboard/users/users.module.css";
 import Link from "next/link";
@@ -25,32 +27,7 @@ const UsersPage = ({ searchParams }) => {
     return (
         <UserProvider>
             <div className={styles.container}>
-                <div className={styles.top}>
-                    {/* <Search placeholder="Search for a user..." />
-                <Link href="/dashboard/users/add">
-                    <button className={styles.addButton}>Add New</button>
-                </Link> */}
-                </div>
-                <div className="bg-gray-100 min-h-screen py-16 text-black">
-                    <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-                        <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={userData ? userData.imgProfile : ''} alt={userData ? userData.Nombre : 'Cargando...'} />
-                        <div className="text-center space-y-2 sm:text-left">
-                            <div className="space-y-0.5">
-                                <p className="text-lg text-black font-semibold">
-                                {userData ? userData.Nombre : 'Cargando...'}
-                                </p>
-                                <p className="text-slate-500 font-medium">
-                                {userData ? userData.role.TipoRol : 'Cargando...'}
-                                </p>
-                            </div>
-                            <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Email: {userData ? userData.Email : 'Cargando...'}</button>
-                        </div>
-                    </div>                
-                </div>
-               
-
-
-
+                <MainInfo/>
             </div>
         </UserProvider>
     );
--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { fetchUsers } from "/app/lib/data.js";
 import MenuLink from "./menuLink/menuLink";
-import styles from "./sidebar.module.css";
-import { MdDashboard, MdSupervisedUserCircle, MdBook, MdQueryStats, MdCalendarMonth, MdOutlineSettings, MdHelpCenter, MdLogout } from "react-icons/md";
+import { MdDashboard, MdSupervisedUserCircle, MdBook, MdQueryStats, MdCalendarMonth, MdOutlineSettings, MdHelpCenter, MdLogout, MdMenu } from "react-icons/md";
 import Link from 'next/link';
+import styles from "./sidebar.module.css";
+import ButtonDashboard from '../button/ButtonDashboard';
 
-const Sidebar = ({ searchParams }) => {
+const Sidebar = ({ searchParams, isOpen, toggleSidebar }) => {
     const [userData, setUserData] = useState(null);
+    const [sidebarWidth, setSidebarWidth] = useState('full'); // Estado para controlar el ancho del sidebar
 
     useEffect(() => {
         const loadUserData = async () => {
@@ -17,7 +19,6 @@ const Sidebar = ({ searchParams }) => {
                 console.error('Error fetching user data:', error);
             }
         };
-
         loadUserData();
     }, [searchParams]);
 
@@ -25,7 +26,7 @@ const Sidebar = ({ searchParams }) => {
 
     const menuItems = [
         {
-            title: "Pages",
+            title: "",
             list: [
                 {
                     title: "Inicio",
@@ -42,10 +43,6 @@ const Sidebar = ({ searchParams }) => {
                     path: "/dashboard/cursos",
                     icon: <MdBook />,
                 },
-            ],
-        },
-        {
-            list: [
                 {
                     title: "Promedio",
                     path: "/dashboard/revenue",
@@ -56,62 +53,62 @@ const Sidebar = ({ searchParams }) => {
                     path: "/dashboard/teams",
                     icon: <MdCalendarMonth />,
                 },
-            ],
-        },
-        {
-            title: "Configuración",
-            list: [
                 {
                     title: "Configurar Cursos",
                     path: "/dashboard/settings",
                     icon: <MdOutlineSettings />,
                 },
-            //     {
-            //         title: "Ayuda",
-            //         path: "/dashboard/help",
-            //         icon: <MdHelpCenter />,
-            //     },
+                {
+                    title: "Cerrar sesión",
+                    path: "/dashboard/settings",
+                    icon: <MdLogout />,
+                },
             ],
         },
     ];
 
     // Si el usuario es un estudiante, ocultar el elemento "Configurar Cursos" del menú
     if (!isTeacher) {
-        menuItems[2].list = menuItems[2].list.filter(item => item.title !== "Configurar Cursos");
+        menuItems[0].list = menuItems[0].list.filter(item => item.title !== "Configurar Cursos");
     }
 
+    // Función para cambiar el ancho del sidebar
+    const handleToggleWidth = () => {
+        setSidebarWidth(sidebarWidth === 'full' ? 'collapsed' : 'full');
+    };
+
     return (
-        <div className={styles.container}>
-            <div className="flex items-center justify-center">
-                <div className=" flex flex-row-reverse flex-nowrap items-end">
-                    <div className={`${styles.userImage} rounded-full overflow-hidden w-24 h-24 mb-3`}>
-                        <img
-                            className="object-cover w-full h-full"
-                            src={userData ? userData.imgProfile : ''}
-                            alt={userData ? userData.name : ''}
-                            width={100}
-                            height={100}
-                        />
-                    </div>
-                    <div className={`${styles.userDetail} p-4`}>
-                        <span>{userData ? userData.Nombre : 'Cargando...'}</span>
-                        <span className={styles.userTitle}>{userData ? userData.role.TipoRol : 'Cargando...'} </span>
+        <div className={`${styles.container} ${styles[sidebarWidth]}`}> {/* Aplica la clase condicional para cambiar el ancho */}
+            <div className="flex items-center justify-between p-4 ">
+                <span className="absolute cursor-pointer right-1 top-[-10px]">
+                    <ButtonDashboard onClick={handleToggleWidth} /> {/* Llama a la función cuando se haga clic */}
+                </span>
+                <div className="flex items-center justify-center">
+                    <div className=" flex flex-row-reverse flex-nowrap items-end">
+                        <div className={`${styles.userImage} rounded-full overflow-hidden w-24 h-24 mb-3`}>
+                            <img
+                                className="object-cover w-full h-full"
+                                src={userData ? userData.imgProfile : ''}
+                                alt={userData ? userData.name : ''}
+                                width={100}
+                                height={100}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-            <ul className={styles.list}>
-                {menuItems.map((cat, index) => (
-                    <li key={index}>
-                        <span className={styles.username}>{userData ? userData.name : ''}</span>
-                        {cat.list.map((item, index) => (
-                            <MenuLink item={item} key={index} />
-                        ))}
-                    </li>
-                ))}
-            </ul>
-            <button className="w-[100%]">
-                <Link className={styles.logout} href='/login'><MdLogout />Cerrar sesión</Link>
-            </button>
+            <div className="flex-1 overflow-y-auto">
+                <ul className="py-4">
+                    {menuItems.map((cat, index) => (
+                        <li key={index}>
+                            <span className={`block w-full text-center md:text-left py-2 px-4 text-sm font-semibold ${isOpen ? 'md:pl-4' : ''}`}>{cat.title}</span>
+                            {cat.list.map((item, index) => (
+                                <MenuLink item={item} key={index} isOpen={isOpen} />
+                            ))}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
